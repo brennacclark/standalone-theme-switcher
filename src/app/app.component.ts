709a04421dc1a64e108ec7758e1cfafac350be05
Brenna Clark <brennacclark@gmail.com>
2023-12-05 19:37:51 -0500

    Added theme to local storage to persist on refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
@@ -10,14 +10,21 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Standalone Theme Switcher';
 
   theme = 'theme-light';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.theme = localStorage.getItem('theme-color') || 'theme-light';
+  }
 
   onThemeChanged(newTheme: string) {
     // Update the theme in local storage
 
     this.theme = newTheme;
+    localStorage.setItem('theme-color', this.theme);
   }
 }
